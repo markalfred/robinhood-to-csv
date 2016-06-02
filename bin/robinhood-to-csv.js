14e4882adc0d968497cd186c13fa904ea1b63218
Mark Battersby <mark@mojotech.com>
2016-06-02 02:33:52 -0400

    Use commander for argument parsing

--- bin/robinhood-to-csv.js
+++ bin/robinhood-to-csv.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
-const argv = require('minimist')(process.argv.slice(2));
+
+const pkg = require('../package.json')
+const program = require('commander')
 
 const main = require('../lib/main')
 
+program
+  .version(pkg.version)
+  .option('-u, --username <username>', 'Username')
+  .option('-p, --password <password>', 'Password')
+  .parse(process.argv)
+
+const { username, password } = program
+
 module.exports =
-  main.login(argv)
+  main.login({ username, password })
     .then(main.getOrders)
     .then(main.getSymbols)
     .then(main.getExecutions)
