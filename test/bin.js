e04de10bfe3a6ace3d49108c5b845769ba0d3645
Mark Battersby <mark@mojotech.com>
2016-10-19 15:24:20 -0400

    [touchup] Yank bin path to a constant

--- test/bin.js
+++ test/bin.js
@@ -26,6 +26,7 @@ const badAuth = {
   password: 'bad password'
 }
 
+const binPath = '../bin/robinhood-to-csv'
 const argvBackup = Array(...process.argv)
 
 describe('Binary', () => {
@@ -84,7 +85,7 @@ describe('Binary', () => {
 
     describe('username', () => {
       it('is accepted in long form', (done) => {
-        requireUncached('../bin/robinhood-to-csv').then(() => {
+        requireUncached(binPath).then(() => {
           main.login.should.have.been.calledWithMatch({ username: 'foo' })
           done()
         })
@@ -93,7 +94,7 @@ describe('Binary', () => {
 
     describe('password', () => {
       it('is accepted in long form', (done) => {
-        requireUncached('../bin/robinhood-to-csv').then(() => {
+        requireUncached(binPath).then(() => {
           main.login.should.have.been.calledWithMatch({ password: 'bar' })
           done()
         })
@@ -111,7 +112,7 @@ describe('Binary', () => {
     afterEach(() => { main.printCsv.restore() })
 
     it('prints csv to stdout', (done) => {
-      requireUncached('../bin/robinhood-to-csv').then(() => {
+      requireUncached(binPath).then(() => {
         main.printCsv.should.have.been.calledWith(output)
         done()
       })
@@ -132,7 +133,7 @@ describe('Binary', () => {
     })
 
     it('throws the errors and exits', (done) => {
-      requireUncached('../bin/robinhood-to-csv').then(() => {
+      requireUncached(binPath).then(() => {
         console.error.should.have.been.calledWithMatch({ data: badLogin })
         process.exit.should.have.been.calledWith(1)
         done()
